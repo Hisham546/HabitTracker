156e8b58f0cd096974b3b202d5f6c43f5b6fd637
Muhammed Hisham <hmuhammed546@gmail.com>
2024-11-26 19:59:22 +0530

    watermelon db intial setup done

--- src/screens/create-habits/index.tsx
+++ src/screens/create-habits/index.tsx
@@ -1,7 +1,10 @@
-import { View, Text } from "react-native"
+import { View, Text } from "react-native";
+import { useState } from "react";
 
-import {ButtonComponent,TextInputOutlined} from "../../components/index";
+import { ButtonComponent, TextInputOutlined } from "../../components/index";
 import styles from "./styles";
+import { Icon } from "../../utils/Icon";
+
 
 
 
@@ -10,12 +13,91 @@ import styles from "./styles";
 const CreateHabits = () => {
 
 
+
+
+
+
+
+
+
+
+
+    const [formData, setFormData] = useState({
+
+        name: '',
+        password: ''
+
+
+
+
+    });
+
+
+
+    const onChangeText = (state: string, value: string | number) => {
+
+
+
+
+
+        setFormData(prevState => ({
+            ...prevState,
+            [state]: value,
+        }));
+
+    };
+
+
+
+
+
     return (
 
         <View style={styles.container}>
 
-            <View style={styles.header}></View>
-            <View style={styles.centerView}></View>
+            <View style={styles.header}>
+                <Icon
+                iconFamily={'AntDesign'}
+                size={23}
+                style={{ color: 'black' }}
+                name={'close'}
+                />
+                <Text style={styles.createHabit}> Create Habit</Text>
+            </View>
+            <View style={styles.centerView}>
+                <TextInputOutlined
+                    labelstyle={styles.labelstyle}
+                    parentContaineStyle={styles.parentContaineStyle}
+
+                    textInputStyle={styles.textInputStyle}
+
+                    headingLabel={'Name'}
+                    showLabel={true}
+
+
+                    placeholderText={"Name"}
+                    keyboardType='default'
+                    onChangeText={value => onChangeText("name", value)}
+
+                    value={formData.name}
+                />
+                <TextInputOutlined
+                    labelstyle={styles.labelstyle}
+                    parentContaineStyle={styles.parentContaineStyle}
+
+                    textInputStyle={styles.textInputStyle}
+
+                    headingLabel={'Description'}
+                    showLabel={true}
+
+
+                    placeholderText={"Description"}
+                    keyboardType='default'
+                    onChangeText={value => onChangeText("name", value)}
+
+                    value={formData.name}
+                />
+            </View>
         </View>
     )
 }
--- src/screens/create-habits/styles.ts
+++ src/screens/create-habits/styles.ts
@@ -2,10 +2,12 @@ import { StyleSheet } from "react-native";
 
 import deviceDimensions from "../../utils/DeviceDimensions";
 import { theme } from "../../styles";
-
+const { colors, fontFamily, fontSize } = theme
+const { deviceHeight, deviceWidth } = deviceDimensions
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#fdfcf7'
     },
 
 
@@ -14,6 +16,57 @@ const styles = StyleSheet.create({
 
     },
     header: {
+        width: deviceWidth,
+        height: deviceHeight * 0.10,
+          alignItems:'center',
+          flexDirection:'row'
+    },
+
+    labelstyle: {
+        fontFamily: fontFamily.P_MEDIUM,
+        color: colors.DARK_SLATE_GRAY,
+    },
+    selectedTextStyle: {
+        color: colors.DARK_SLATE_GRAY,
+        fontSize: fontSize.p,
+        marginLeft: 10,
+        fontFamily: fontFamily.P_REGULAR
+    },
+    placeholderStyles: {
+        color: colors.DARK_SLATE_GRAY,
+        fontFamily: fontFamily.P_REGULAR,
+        fontSize: fontSize._13
+    },
+    parentContaineStyle: {
+        height: deviceDimensions.deviceHeight * 0.12,
+        width: deviceDimensions.deviceWidth,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    textInputStyle: {
+        height: deviceDimensions.deviceHeight * 0.056,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: colors.GRAY_20,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        flexDirection: 'row',
+        width: deviceDimensions.deviceWidth * 0.80,
+        backgroundColor: colors.WHITE,
+        color: colors.WHITE
+    },
+    descriptionView: {
+        width: deviceDimensions.deviceWidth,
+        height: deviceDimensions.deviceHeight * 0.40,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    createHabit: {
+        fontSize: fontSize.h5,
+        color: colors.BLACK,
+      
 
     }
 
